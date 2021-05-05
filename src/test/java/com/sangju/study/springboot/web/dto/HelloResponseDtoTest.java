1a67733a65ddf683b342a6293d67c6cb28950a34
sangju <hyun24.dev@gmail.com>
2021-05-05 21:59:15 +0900

    Dto JUinit add

---
+++ src/test/java/com/sangju/study/springboot/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,22 @@
+package com.sangju.study.springboot.web.dto;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void lombokActionTest() {
+
+        String name = "test";
+        int amount = 1000;
+
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+
+    }
+
+}
