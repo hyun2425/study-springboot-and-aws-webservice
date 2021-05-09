fc1b24169420a6facfb2b78041666db731624e47
sangju <hyun24.dev@gmail.com>
2021-05-09 21:22:26 +0900

    Posts api add v1

--- src/main/java/com/sangju/study/springboot/web/PostsApiController.java
+++ src/main/java/com/sangju/study/springboot/web/PostsApiController.java
@@ -1,11 +1,11 @@
 package com.sangju.study.springboot.web;
 
 import com.sangju.study.springboot.service.posts.PostsService;
+import com.sangju.study.springboot.web.dto.PostsResponseDto;
 import com.sangju.study.springboot.web.dto.PostsSaveRequestDto;
+import com.sangju.study.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -15,7 +15,17 @@ public class PostsApiController {
 
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
-//        return postsService.save(requestDto);
-        return null;
+        return postsService.save(requestDto);
     }
+
+    @PostMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+
 }
