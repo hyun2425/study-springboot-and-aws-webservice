4e66715eb2eb65a08f5287629710d69394b54122
sangju <hyun24.dev@gmail.com>
2021-05-10 21:52:36 +0900

    Posts BaseTimeEntity add

--- src/test/java/com/sangju/study/springboot/web/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/sangju/study/springboot/web/domain/posts/PostsRepositoryTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -46,4 +47,28 @@ public class PostsRepositoryTest {
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
     }
+
+    @Test
+    public void BaseTimeEntity_등록() {
+        //given
+        LocalDateTime now = LocalDateTime.of(2021, 5, 10, 20, 0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>> createDate=" + posts.getCreaDate());
+        System.out.println(">>>>>> modifienDate=" + posts.getModiDate());
+
+        assertThat(posts.getCreaDate()).isAfter(now);
+        assertThat(posts.getModiDate()).isAfter(now);
+    }
+
 }
