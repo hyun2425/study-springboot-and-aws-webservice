918bfe9b7a62c529b8aeff66c3c3daf82ead267d
hyun2425 <hyun2425>
2021-05-27 17:27:32 +0900

    google oauth add

--- src/main/java/com/sangju/study/springboot/web/IndexController.java
+++ src/main/java/com/sangju/study/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.sangju.study.springboot.web;
 
+import com.sangju.study.springboot.config.auth.dto.SessionUser;
 import com.sangju.study.springboot.service.posts.PostsService;
 import com.sangju.study.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +9,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
@@ -29,7 +37,6 @@ public class IndexController {
     public String postsUpdate(@PathVariable Long id, Model model) {
         PostsResponseDto dto = postsService.findById(id);
         model.addAttribute("post", dto);
-
         return "posts-update";
     }
 }
