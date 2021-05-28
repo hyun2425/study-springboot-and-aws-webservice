c0091d6f3efe740fb6ddede643a9d47a4cb62ff8
hyun2425 <hyun2425>
2021-05-28 18:44:13 +0900

    google Naver oauth add

--- src/main/java/com/sangju/study/springboot/web/IndexController.java
+++ src/main/java/com/sangju/study/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.sangju.study.springboot.web;
 
+import com.sangju.study.springboot.config.auth.LoginUser;
 import com.sangju.study.springboot.config.auth.dto.SessionUser;
 import com.sangju.study.springboot.service.posts.PostsService;
 import com.sangju.study.springboot.web.dto.PostsResponseDto;
@@ -16,12 +17,10 @@ import javax.servlet.http.HttpSession;
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
